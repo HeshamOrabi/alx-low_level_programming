d9b3a8f09cca00d957c9fc2f3f0e6231af8847f5
Hesham <Dev@heshamorabi.com>
2023-06-15 08:19:07 -0700

    Fix a bug

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    	write(STDERR_FILENO, message, sizeof(message) - 1);
+	const char m[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    	write(STDERR_FILENO, m, sizeof(m) - 1);
 	return (1);
 }
