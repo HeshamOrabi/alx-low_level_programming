632bd9d1c818b48519167d19b3c79d7698aa48f4
Hesham <Dev@heshamorabi.com>
2023-06-15 06:05:54 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
-# include <stdio.h>
+#include <stdio.h>
 
-int main(void) {
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
