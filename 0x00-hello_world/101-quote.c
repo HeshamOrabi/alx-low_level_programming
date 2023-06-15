cef39702af7c7985e37e0f032e33b26050f45cc9
Hesham <Dev@heshamorabi.com>
2023-06-15 08:24:50 -0700

    Fix a bug

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,9 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO,m,sizeof(m)-1);
-	return (1);
+  char m[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  write(STDERR_FILENO, m, sizeof(m) - 1);
+  return (0);
 }
