b7975a86a2caa21ef6f3bd047a18fb589a932818
Hesham <Dev@heshamorabi.com>
2023-06-15 08:06:26 -0700

    C program that prints to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    	write(STDERR_FILENO, message, sizeof(message) - 1);
+	return (1);
+}
