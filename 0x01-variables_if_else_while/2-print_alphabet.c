445909415da366336144887fa86c29293571cfbe
Hesham <Dev@heshamorabi.com>
2023-06-16 09:15:47 -0700

    alphabet cout

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the starting function
+ *
+ * Return: is always 0 (success)
+*/
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		putchar('\n');
+		ch++
+	}
+	return (0);
+}
