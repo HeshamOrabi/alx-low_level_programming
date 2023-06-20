ba4574e93ef9f35765a1394c65d2f5809a891a28
Hesham <Dev@heshamorabi.com>
2023-06-20 15:05:13 -0700

    Fix negative

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,8 +7,16 @@
  */
 int print_last_digit(int n)
 {
-	n = n % 10;
-	_putchar(n + '0');
+	int a;
 
-	return (n);
+	if (n < 0)
+	{
+		n = -n;
+	}
+
+	a = n % 10;
+	_putchar(a + '0');
+
+	return (a);
+
 }
