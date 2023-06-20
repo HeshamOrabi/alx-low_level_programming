060898e082d09ae8acd0b4c33a4f8b16a111225f
Hesham <Dev@heshamorabi.com>
2023-06-20 14:33:01 -0700

    print last_digit with convert

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * print_last_digit - its in the name
- * @n: integertoprint last num
- * Return: 0
-*/
+ * print_last_digit - prints the last digit of a number and returns it
+ * @n: the number to extract the last digit from
+ * Return: the last digit of the input number
+ */
 int print_last_digit(int n)
 {
 	int ld;
 
 	ld = n % 10;
 	_putchar(ld + '0');
-	_putchar('\n');
 
-	return (0);
+	return (ld);
 }
