c27a7074c42d003edf347f3dd9e922a2a1215125
Hesham <Dev@heshamorabi.com>
2023-06-20 04:24:05 -0700

    print with printf

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - this function is the starting of the program
+ *
+ * Ruturn: 0 always (success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+
+	printf("%s", str);
+	return (0);
+}
