11564d4b68ce13ea16037a8f5fc6908d0104456d
Hesham <Dev@heshamorabi.com>
2023-06-20 15:51:42 -0700

    / then %

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints the the day of jack
+ *
+ * Return: nothing
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(':');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,6 +10,7 @@
 	int _abs(int);
 	int print_last_digit(int);
 	void jack_bauer(void);
+	void jack_bauer(void);
 
 #endif
 
