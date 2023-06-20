4937336adfc31e2a56379cb59b04150f8cdeed95
Hesham <Dev@heshamorabi.com>
2023-06-20 10:11:44 -0700

    check numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - is a function to print a sign og a number
+ * @n: the integer number to check
+ * Return: 1 if n is greater than zero, 0 if n is zero, and -1 if n is less than zero
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-')
+		return (-1);
+	}
+}
