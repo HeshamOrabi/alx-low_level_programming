8cd92afce36fcafddb2bde77ef3fbc85a6f3b586
Hesham <Dev@heshamorabi.com>
2023-06-20 05:13:13 -0700

    Final Fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
 #include "main.h"
 
 /**
- * main - this function is the starting of the program
+ * main - Entry point
  *
- * Ruturn: 0 always (success)
+ * Return: 0 always (success)
 */
 
 int main(void)
 {
-	_putchar("_putchar");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 	return (0);
 }
