a23d0c91cbf47f69add0c3edaed8dac7aa028ea2
Hesham <Dev@heshamorabi.com>
2023-06-21 03:43:44 -0700

    print 3 number with putchar

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+*/
+void print_times_table(int n)
+{
+	int i, j,k;
+
+	if (n > 0 && n < 15)
+	{
+		for (i = 0; i < n + 1; i++)
+		{
+			for (j = 0; j < n + 1; j++)
+			{
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				}
+				else if (k < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k / 10 + '0');
+					_putchar(k % 10 + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(k / 100 + '0');
+					_putchar(k / 10 % 10 + '0');
+					_putchar(k % 10 + '0');
+				}
+			}
+			_putchar('\n')
+		}
+	}
+}
