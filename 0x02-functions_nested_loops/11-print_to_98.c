b29ccb6e0b00bb7cdcb3d832ee12e81645fb5385
Hesham <Dev@heshamorabi.com>
2023-06-20 17:21:50 -0700

    Fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,8 +3,7 @@
 
 
 	/**
-	 * print_to_98 - prints all natural numbers from n to 98,
-	 * followed by a new line
+	 * print_to_98 - prints all natural numbers from n 
 	 * @n: print from this number
 	 * Return: no return value
 	 */
