6e5eccbc2ec0e346ff53911bd322ea78d0e78989
Hesham <Dev@heshamorabi.com>
2023-06-22 11:13:20 -0700

    a little fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,33 +2,36 @@
 
 /**
  * main - intialize program
- * 
  * Return: always 0 (success)
 */
 int main(void)
 {
 	int i;
-	
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (((i % 3) == 0) && ((i % 5) == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
 			printf("%d ", i);
 		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
 	}
 	printf("\n");
 
-	return(0);
+	return (0);
 }
