9499d04f290e446b2bfdb785f5a879de7c7b57f1
Hesham <Dev@heshamorabi.com>
2023-06-22 11:06:29 -0700

    #include <stdio.h>

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - intialize program
+ * 
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+	int i;
+	
+	for (i = 1; i <= 100; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+
+	return(0);
+}
