f9ce5fc58bb290e9d9a37475246bcf44f4332cdd
Hesham <Dev@heshamorabi.com>
2023-06-22 06:11:01 -0700

    Fix betty style

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
-/*
- * more_numbers - print numbers from 0 to 14 for 10 times
-*/
+/**
+ * more_numbers - prints numbers from 0 to 14, 10 times
+ */
 void more_numbers(void)
 {
 	int i, j;
