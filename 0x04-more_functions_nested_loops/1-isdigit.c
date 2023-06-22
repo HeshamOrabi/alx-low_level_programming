8dc69c0cf67f264cf24b36598797c1dea69e58cf
Hesham <Dev@heshamorabi.com>
2023-06-22 04:09:51 -0700

    check if digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - to check upper case
+ * @c: int represent ASCII
+ * Return: 1 if upper and 0 otherwise
+*/
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
