7fe327240d7601fe8782610331b4d4ac8951745f
Hesham <Dev@heshamorabi.com>
2023-06-26 13:43:56 -0700

    print str

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,10 @@
-#ifndef main_h
+#ifndef main_has0asa
 #define main_h
 
 	void reset_to_98(int *n);
 	void swap_int(int *a, int *b);
 	int _strlen(char *s);
+	int _putchar(char c);
+	void _puts(char *str);
 
 #endif /* main_h */
