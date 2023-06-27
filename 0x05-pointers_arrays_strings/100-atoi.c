31a1f36f6119bd94fb45ee1513fa537f9f4a733c
Hesham <Dev@heshamorabi.com>
2023-06-26 19:36:21 -0700

    Try

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,19 +10,26 @@ int _atoi(char *s)
 {
 	int i = 0, n = 0, sign = 1;
 
-	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+	while (s[len] != '\0')
 	{
+		len++;
+	}
+
+	while (i < len)
+	{
+		if (s[i] == '-')
+		{
+			sign = -sign;
+		}
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			n = n * 10 + (s[i] - '0') * sign;
+			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
+			break;
+		}
 		i++;
 	}
 
-	if (s[i] == '+' || s[i] == '-')
-	{
-		sign = s[i++] == '-' ? -1 : 1;
-	}
-
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		n = n * 10 + (s[i++] - '0') * sign;
-	}
 	return (n);
 }
