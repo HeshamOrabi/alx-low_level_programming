091b778bce60780ccda95c5376053e2fa43d0d31
Hesham <Dev@heshamorabi.com>
2023-06-26 18:09:49 -0700

    print str

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+*/
+void puts_half(char *str)
+{
+	int i, j, k, len;
+
+	len = 0;
+	for (i = 0; str[i] != 0; i++)
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (j = len / 2; j < len; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	else
+	{
+		for (k = (len - 1) / 2; k < len; k++)
+		{
+			_putchar(str[k]);
+		}
+	}
+	_putchar('\n');
+}
