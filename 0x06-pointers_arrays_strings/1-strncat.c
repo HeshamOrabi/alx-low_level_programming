897bff8712a7cd3a4d8e7528b0336afe32c4c9c6
Hesham <Dev@heshamorabi.com>
2023-06-29 07:06:35 -0700

    try diff code strc

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,20 +11,22 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j, len;
+	int i;
+	int j;
 
-	len = 0;
-	for (i = 0; dest[i] != '\0'; i++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		len++;
+		i++;
 	}
-
-	for (j = 0; src[j] != '\0' && j < n; j++)
+	j = 0;
+	while (j < n && src[j] != '\0')
 	{
-		dest[len + j] = src[j];
+	dest[i] = src[j];
+	i++;
+	j++;
 	}
-
-	dest[len + j] = '\0';
-
+	dest[i] = '\0';
 	return (dest);
 }
+
