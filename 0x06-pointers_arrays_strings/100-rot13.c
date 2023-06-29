8289f6effd86e4f5fac029cc60a4cc2102bc1320
Hesham <Dev@heshamorabi.com>
2023-06-29 16:20:48 -0700

    Fix

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,13 +14,13 @@ char *rot13(char *str)
 	char tmpStr[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char altTempStr[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
-			if (s[i] == tmpStr[j])
+			if (str[i] == tmpStr[j])
 			{
-				s[i] = altTempStr[j];
+				str[i] = altTempStr[j];
 				break;
 			}
 		}
