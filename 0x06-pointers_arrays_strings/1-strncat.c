c529b307e7f3ccb608949088ca73440c71d4ee18
Hesham <Dev@heshamorabi.com>
2023-06-29 06:35:54 -0700

    concatinate spasific num of char in str

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - concat a spasific num n of char in str
+ * @dest: the main str
+ * @src: the str to b econcat
+ * @n: num of char to concat
+ *
+ * Return: dest str
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j, k, len;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		if (j <= n)
+		{
+			dest[j + len] = src[j]
+		}
+		else
+		{
+			dest[j + len] = '/0'
+			break;
+		}
+	}
+
+	return (dest);
+}
