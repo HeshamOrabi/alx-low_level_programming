818e0965bde73c5518b2ba750e4361144f66660c
Hesham <Dev@heshamorabi.com>
2023-07-03 10:48:47 -0700

    Fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,10 +14,14 @@ char *_strchr(char *s, char c)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == c)
+		if (s[i] == '\0')
+		{
+			return (s);
+		}
+		else if (s[i] == c)
 		{
 			return (&s[i]);
 		}
 	}
-	return (NULL);
+	return ('\0');
 }
