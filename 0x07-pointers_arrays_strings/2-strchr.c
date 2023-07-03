aae2fa13695eae911f4f959b4c1f7267c2622ada
Hesham <Dev@heshamorabi.com>
2023-07-03 10:38:33 -0700

    locate a char in str

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		else
+		{
+			return (NULL);
+		}
+	}
+}
