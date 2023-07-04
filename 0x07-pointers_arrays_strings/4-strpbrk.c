c03b76226cf3b90e41236b4d0c8fedb7f8c18781
Hesham <Dev@heshamorabi.com>
2023-07-04 06:58:24 -0700

    check first inst of a num in str

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,11 +12,6 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 
-	if (s == '\0' || accept == '\0')
-	{
-		return (NULL);
-	}
-
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		for (j = 0; *(accept + j) != '\0'; j++)
