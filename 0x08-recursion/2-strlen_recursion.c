ef8f826f85137d2d4608821ed16b9fdf079ba08a
Hesham <Dev@heshamorabi.com>
2023-07-05 16:34:22 -0700

    Try

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,12 @@ int _strlen_recursion(char *s)
 {
 	int len = 0;
 
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		return (len);
+		len++;
 	}
 
-	len++;
-
 	_strlen_recursion(s + 1);
+
+	return (len / 2);
 }
