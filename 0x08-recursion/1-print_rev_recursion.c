87eb85784a82ce1f0e42a8cb4f862b5df932a5f0
Hesham <Dev@heshamorabi.com>
2023-07-05 16:24:00 -0700

    edit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints a string in reverse order, followed by a new line
+ * _print_rev_recursion - prints a string in reverse order
  * @s: the string to be printed in reverse
 */
 void _print_rev_recursion(char *s)
