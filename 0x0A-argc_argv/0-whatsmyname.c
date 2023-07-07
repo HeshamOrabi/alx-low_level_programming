e1cdee206ddc3fc361b03461a5e644fde93825af
Hesham <Dev@heshamorabi.com>
2023-07-07 15:44:39 -0700

    Try

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,12 +7,10 @@
  *
  * Return: 0 if successful
 */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	if (argc > 1)
-	{
-		printf("%s\n", argv[argc - 1]);
-	}
+	(void)argc;
 
+	printf("%s\n", argv[0]);
 	return (0);
 }
