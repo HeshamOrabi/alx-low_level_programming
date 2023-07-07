5199d045507c0603c71a1e952aec4ad85f6ff537
Hesham <Dev@heshamorabi.com>
2023-07-07 15:39:17 -0700

    Print file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 if successful
+*/
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		printf("%s", argv[argc - 1]);
+	}
+
+	return (0);
+}
