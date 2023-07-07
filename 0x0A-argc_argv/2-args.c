4343e0e9bc08fe52c4940d4f27e234d0010ab883
Hesham <Dev@heshamorabi.com>
2023-07-07 15:55:27 -0700

    Print arg in argv arr

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all arguments passed into the File
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 if successful
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (i = 0; i < argc; i++)
+	{	
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
