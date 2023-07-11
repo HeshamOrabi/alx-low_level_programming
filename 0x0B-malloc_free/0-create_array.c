bfedfd964d7ddeecc089bc8ff3b3a27974397521
Hesham <Dev@heshamorabi.com>
2023-07-11 12:53:43 -0700

    Creates an array in heap

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * create_array - creates an array, and initializes it with a char
+ * @size: size of the array
+ * @c: char to initialize the array with
+ *
+ * Return: pointer to the array, or NULL if size is 0 or if malloc fails
+*/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *p;
+
+	if (size <= 0)
+	{
+		return (NULL);
+	}
+	p = (char *)malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+	{
+		*(p + i) = c;
+	}
+
+	return (p);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef main_h
+#define main_h
+
+	char *create_array(unsigned int size, char c);
+
+#endif
