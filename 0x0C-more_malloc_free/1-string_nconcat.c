19e2484e9815dc6b5481c2942d796bb17ffe9408
Hesham <Dev@heshamorabi.com>
2023-07-13 17:57:12 -0700

    concate str

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,46 +13,36 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *arr;
+	char *s;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while (s1[len1] != '\0')
-	{
+	while (s1 && s1[len1])
 		len1++;
-	}
-	while (s2[len2] != '\0')
-	{
+	while (s2 && s2[len2])
 		len2++;
-	}
 
 	if (n < len2)
-	{
-		arr = malloc(sizeof(char) * (len1 + n + 1));
-	}
+		s = malloc(sizeof(char) * (len1 + n + 1));
 	else
-	{
-		arr = malloc(sizeof(char) * (len1 + len2 + 1));
-	}
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (arr == NULL)
-	{
+	if (!s)
 		return (NULL);
-	}
 
 	while (i < len1)
 	{
-		arr[i] = s1[i];
+		s[i] = s1[i];
 		i++;
 	}
 
 	while (n < len2 && i < (len1 + n))
-		arr[i++] = s2[j++];
+		s[i++] = s2[j++];
 
 	while (n >= len2 && i < (len1 + len2))
-		arr[i++] = s2[j++];
+		s[i++] = s2[j++];
 
-	arr[i] = '\0';
+	s[i] = '\0';
 
-	return (arr);
-
+	return (s);
 }
+
