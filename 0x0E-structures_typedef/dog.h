a849139d6f373180ad9252ddc3b7899e298dcc54
Hesham <Dev@heshamorabi.com>
2023-07-17 16:50:56 -0700

    Fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,11 +8,24 @@
  *
  * Description: Longer description
 */
-struct dog{
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
 
+/**
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
+
 #endif
