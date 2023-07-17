5138a02fece20456c0f83cddeb94a0e20eb76af3
Hesham <Dev@heshamorabi.com>
2023-07-17 16:45:35 -0700

    MAke a readme

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+*/
 struct dog{
 	char *name;
 	float age;
