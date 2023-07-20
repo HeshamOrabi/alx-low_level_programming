3dea5f987d54e83d5814fa07ba506380102a3a31
Hesham <Dev@heshamorabi.com>
2023-07-20 12:49:53 -0700

    poiter to fun call

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,6 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL || size == 0 || action == NULL)
 		return;
+
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
