c525a53d6409cae959bad016a747f338441c634b
Hesham <Dev@heshamorabi.com>
2023-07-20 12:18:27 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Applies a function to a name string
+ * @name: pointer to a character array containing the name
+ * @f: pointer to a function that takes a pointer to a character array
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
