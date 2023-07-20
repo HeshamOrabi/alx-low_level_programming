702f0ba623b88a4d126872870229a1fc75049b7a
Hesham <Dev@heshamorabi.com>
2023-07-20 14:40:48 -0700

    Fix -> sum all arg

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,13 +11,13 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int num, sum;
+	va_list args;
 
 	if (n == 0)
 	{
 		return 0;
 	}
 
-	va_list args;
 	va_start(args, n);
 	
 	for (i = 0; i < n; i++)
