1fcf9c5a29b9c0a89b4f6666ea470a4b0cc35b77
Hesham <Dev@heshamorabi.com>
2023-07-20 12:30:30 -0700

    Fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
  * print_name - Applies a function to a name string
  * @name: pointer to a character array containing the name
@@ -8,9 +8,8 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-	{
 		return;
-	}
 
 	f(name);
 }
+
