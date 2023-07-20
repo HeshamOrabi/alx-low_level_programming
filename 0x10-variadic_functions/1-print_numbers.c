07a93557481512e2be94fcb0f8c7f9b697a48d0a
Hesham <Dev@heshamorabi.com>
2023-07-20 15:07:28 -0700

    Try

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,7 +18,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		rnum =	va_arg(args, int);
-		if (separator == NULL)
+		if (i == (n - 1) || separator == NULL)
 		{
 			printf("%d", rnum);
 		}
