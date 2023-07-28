24ad692307df1afaf447fdb8b8bcc194913d09eb
Hesham <dev@heshamorabi.com>
2023-07-28 12:02:10 -0700

    Fix

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,8 +8,7 @@
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newNode;
-	list_t *tmp;
+	list_t *newNode, *tmp;
 	int len = 0;
 
 	while (str[len])
@@ -27,13 +26,13 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	newNode->next = NULL;
 
-	
 	if (*head == NULL)
 	{
 		*head = newNode;
 		return (newNode);
 	}
 
+	tmp = *head;
 	while (tmp->next)
 	{
 		tmp = tmp->next;
