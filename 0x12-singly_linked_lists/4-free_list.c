893f68233d72a06669d54bf19992e079d8371bc3
Hesham <dev@heshamorabi.com>
2023-07-28 12:10:32 -0700

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list from heap
+ * @head: list_t list to be freed
+*/
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head->len);
+		free(head->next);
+		free(head);
+		head = temp;
+	}
+}
+
