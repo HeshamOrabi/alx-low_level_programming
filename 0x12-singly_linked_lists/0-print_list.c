623ed2725ec00fe899eeb9aebe19d978ec5adc10
Hesham <dev@heshamorabi.com>
2023-07-28 09:22:49 -0700

    func to print elem in linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,51 @@
+#include "list_h"
+
+/**
+ * print_list - print content of a list
+ * @h : pointer to head of a list
+ *
+ * Return: number of nodes in a list
+*/
+size_t print_list(const list_t *h)
+{
+	int i;
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		i = 0;
+
+		if (h->str)
+		{
+			_putchar('[');
+			_putchar((h->len) + '0');
+			_putchar(']');
+			_putchar(' ');
+		}
+
+		if (!h->str)
+		{
+			_putchar('[');
+			_putchar('0');
+			_putchar(']');
+			_putchar(' ');
+			_putchar('(');
+			_putchar('n');
+			_putchar('i');
+			_putchar('l');
+			_putchar(')');
+		}
+		else
+		{
+			while (h->str[i])
+			{
+				_putchar(h->str[i]);
+				i++;
+			}
+		}
+		_putchar('\n');
+		h = h->next;
+	}
+	return (count);
+}
