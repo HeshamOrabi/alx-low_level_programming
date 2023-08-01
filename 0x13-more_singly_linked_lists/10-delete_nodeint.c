4670b49b1d8ca65177c45ef36e1162dc25614a47
Hesham <dev@heshamorabi.com>
2023-08-01 15:00:45 -0700

    delete node at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,7 +9,8 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp, *del;
+	listint_t *temp, *del = NULL;
+	unsigned int i = 0;
 
 	while (!head || !(*head))
 		return (-1);
@@ -22,13 +23,13 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = temp;
 	}
 
-	while (index - 1)
+	while (i < index - 1)
 	{
 		if (!temp || !(temp->next))
 			return (-1);
 
 		temp = temp->next;
-		index--;
+		i++;
 	}
 	del = temp->next;
 	temp->next = del->next;
