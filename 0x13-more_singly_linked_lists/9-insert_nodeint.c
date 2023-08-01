416cddb13aa7f9164ffadf4f6be3da942373fe98
Hesham <dev@heshamorabi.com>
2023-08-01 14:25:47 -0700

    insert a node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,19 +32,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	temp = *head;
-	while (index > i)
+
+	for (i = 0; temp && i < idx; i++)
 	{
-		temp = temp->next;
-		index--;
+		if (i == index)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
 	}
 
-	if (!temp)
-	{
-		return (NULL);
-	}
+	return (NULL);
 
-	new->next = temp->next;
-	temp->next = new;
-
-	return (new);
 }
