6d9fed0b688d30b8c06bf34b6976074af81d5460
Hesham <dev@heshamorabi.com>
2023-08-03 17:15:40 -0700

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts of chaged bit
+ * @n: number
+ * @m: number
+ *
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, count = 0;
+	unsigned long int val;
+	unsigned long int ex = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		val = ex >> i;
+		if (current & 1)
+			count++;
+	}
+
+	return (count);
+}
