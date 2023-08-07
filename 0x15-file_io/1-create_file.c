b6fd531ed838d93c1110547017ad4a927622ea45
Hesham <dev@heshamorabi.com>
2023-08-07 06:46:27 -0700

    Hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len, wr;
+	int fd, len = 0, wr;
 
 	if (filename == NULL)
 		return (-1);
@@ -20,8 +20,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (text_content != '\0')
-			len++
+		while (text_content[len] != '\0')
+			len++;
 	}
 
 	wr = write(fd, text_content, len);
