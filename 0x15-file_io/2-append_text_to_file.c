46a8665332a02063e4bd74c8eaa15cbe078d5455
Hesham <dev@heshamorabi.com>
2023-08-07 07:11:17 -0700

    apped text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
 /**
+ * append_text_to_file - append text at end of file
+ * @filename: str of file name
+ * @text_content: str to append
+ *
+ * Return: 1 succes, -1 fail
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
