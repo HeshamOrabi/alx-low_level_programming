6d79b25feeb9a252b1779c2baddf520651e0279d
Hesham <dev@heshamorabi.com>
2023-08-07 05:59:09 -0700

    Fix -> pr test to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t nr, nw;
-	char buf[letters];
+	char *buf;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
@@ -19,15 +19,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
+	buf = malloc(sizeof(char) * letters);
+
 	nr = read(fd, buf, letters);
 
 
-	nw = write(1, buf, nr);
+	nw = write(STDOUT_FILENO, buf, nr);
 
 	if (nr != nw)
 	{
 		return (0);
 	}
+
+	free(buf);
+	close(fd);
 
 	return (nw);
 }
