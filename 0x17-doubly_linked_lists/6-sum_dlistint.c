3d35fcb202a57a7be77b2bd61ab3d9d1beb72955
Hesham <dev@heshamorabi.com>
2023-09-14 13:32:34 -0700

    Sum list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -5,10 +5,10 @@
  * sum_dlistint - Calculates the sum of all elements in a doubly linked list
  * @head: Pointer to the head of the list
  *
- * Description: This function calculates the sum of all the integer values stored
+ * Description: calculates the sum of all the integer values stored
  * in the nodes of a doubly linked list.
  *
- * Return: The sum of all elements in the list. If the list is empty, returns 0.
+ * Return: The sum of all elements in the list. If empty returns 0.
 */
 int sum_dlistint(dlistint_t *head)
 {
