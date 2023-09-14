3e0a8a70fdd36d1355934c15d0a716a628f60c10
Hesham <dev@heshamorabi.com>
2023-09-14 14:46:48 -0700

    Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,47 +1,52 @@
-#include "lists.h"
-
 /**
- * insert_dnodeint_at_index - Inserts a new node at a specific index
- * @h: Pointer to a pointer to the head of the list
- * @idx: Index at which to insert the new node
- * @n: Integer value to be stored in the new node
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
  *
- * Description: This function inserts a new node with integer value 'n' at
- * the specified index. If the index is out of range, it returns NULL.
- * If the list is empty and idx is not 0, it also returns NULL. If insertion
- * is successful, it returns a pointer to the new node.
- *
- * Return: If the insertion is successful, returns a pointer to the new node.
- * If the index is out of range or memory allocation fails, returns NULL.
-*/
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *tmp = *h;
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
+	new = NULL;
 	if (idx == 0)
 		new = add_dnodeint(h, n);
-
-	while (idx && tmp)
+	else
 	{
-		tmp = tmp->next;
-		idx--;
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
+		{
+			if (i == idx)
+			{
+				if (head->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
+				}
+				break;
+			}
+			head = head->next;
+			i++;
+		}
 	}
-	if (!tmp)
-	{
-		new = add_dnodeint_end(h, n);
-	}
-
-	new = malloc(sizeof(dlistint_t));
-	if (!new)
-		return (NULL);
-
-	new->n = n;
-
-	new->next = tmp;
-	(tmp->prev)->next = new;
-	tmp->prev = new;
-	new->prev = tmp->prev;
 
 	return (new);
-
 }
