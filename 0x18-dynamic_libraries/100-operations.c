927dff24f42ec8094e46a3712037adeb81544738
Hesham <dev@heshamorabi.com>
2023-09-24 07:35:58 -0700

    error confilict div

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,66 @@
+#include <Python.h>
+
+/**
+ * add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The sum of a and b.
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - Subtracts two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of a minus b.
+*/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The product of a and b.
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - Divides two integers.
+ * @a: The numerator.
+ * @b: The denominator.
+ *
+ * Return: The result of a divided by b.
+*/
+int div(int a, int b)
+{
+	if (b == 0)
+		return (0);
+
+	return a / b;
+}
+
+/**
+ * mod - Computes the modulus of two integers.
+ * @a: The dividend.
+ * @b: The divisor.
+ *
+ * Return: The remainder of a divided by b.
+ */
+int mod(int a, int b)
+{
+	if (b == 0)
+		return (-1);
+	return (a % b);
+}
