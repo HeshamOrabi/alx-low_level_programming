bc5b6b3cd41f97d1c4ce84e0dc417e72c7366cd3
Hesham <dev@heshamorabi.com>
2023-10-06 15:58:22 -0700

    >>> ht[betty] = cool

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,6 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
+	new_node->next = NULL;
 	if (!new_node->key || !new_node->value)
 	{
 		free(new_node->key);
@@ -26,7 +27,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-	new_node->next = NULL;
 
 	index = key_index((unsigned char *)key, ht->size);
 	if ((ht->array[index]) == NULL)
