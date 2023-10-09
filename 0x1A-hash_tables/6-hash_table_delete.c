fc478f5e25c2a06d9b6e6dd6d974c9a56ab55095
Hesham <dev@heshamorabi.com>
2023-10-09 14:53:44 -0700

    >>> del ht

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: The hash table to be deleted.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *current;
+	hash_node_t *next;
+	unsigned long int i;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current)
+		{
+			next = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = next;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
