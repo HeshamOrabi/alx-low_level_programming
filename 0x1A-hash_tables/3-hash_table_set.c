6f3075394715843c255525e67ae6b3f5321422e8
Hesham <dev@heshamorabi.com>
2023-10-09 11:25:26 -0700

    mind

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,22 +1,39 @@
 #include "hash_tables.h"
 
 /**
-*/
+ * hash_table_set - Adds an element to a hash table.
+ * @ht: The hash table to add or update the element in.
+ * @key: The key to identify the element.
+ * @value: The value associated with the key.
+ *
+ * Return: 1 if successful, 0 otherwise.
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *hn;
 	unsigned long int index;
 	hash_node_t *tmp;
 
-	if (!ht)
+	if (!ht || !key || !value)
 		return (0);
 
 	hn = malloc(sizeof(hash_node_t));
 	if (!hn)
 		return (0);
 
-	hn->key = (char *)key;
-	hn->value = (char *)value;
+	hn->key = strdup(key);
+	if (!hn->key)
+	{
+		free(hn);
+		return (0);
+	}
+	hn->value = strdup(value);
+	if(!hn->value)
+	{
+		free(hn->key);
+		free(hn);
+		return (0);
+	}
 	hn->next = NULL;
 
 	index = key_index((unsigned char *)key, ht->size);
@@ -32,7 +49,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		else
 		{
 			tmp = (ht->array[index])->next;
-			if (!tmp->next)
+			while (tmp->next)
 				tmp = tmp->next;
 
 			tmp->next = hn;
