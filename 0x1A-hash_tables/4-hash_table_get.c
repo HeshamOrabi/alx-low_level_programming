37adc80893eecb870bdd2b40d250dc7254f672c0
Hesham <dev@heshamorabi.com>
2023-10-09 13:12:07 -0700

    Hot fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,6 +10,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *node;
 
 	if (!ht || key[0] == '\0' || !key)
 		return (NULL);
@@ -18,6 +19,15 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	if (!ht->array[index])
 		return (NULL);
+	else
+	{
+		node = ht->array[index];
+		while (!node && strcmp(node->key, key))
+			node = node->next;
+		if (!node)
+			return (NULL);
+		else
+			return (node->value);
+	}
 
-	return (ht->array[index]->value);
 }
