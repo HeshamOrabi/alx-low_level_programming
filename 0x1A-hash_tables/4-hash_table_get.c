ae08807557dfe5d9659b70e9e2bea84a79c6d3ac
Hesham <dev@heshamorabi.com>
2023-10-09 12:40:04 -0700

    fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,10 +1,18 @@
 #include "hash_tables.h"
 
 /**
-*/
+ * hash_table_get - Retrieves the value associated with a given key.
+ * @ht: The hash table to search.
+ * @key: The key to look for in the hash table.
+ *
+ * Return: The value associated with the key if found, otherwise NULL.
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+
+	if (!ht || key[0] == '\0' || !key)
+		return (NULL);
 
 	index = key_index((unsigned char *)key, ht->size);
 
