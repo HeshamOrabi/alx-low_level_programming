c887cccdb074617432db8c56b6a19a0a3686a71c
Hesham <dev@heshamorabi.com>
2023-10-09 13:16:53 -0700

    test

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,22 +12,19 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node;
 
-	if (!ht || key[0] == '\0' || !key)
+	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
 		return (NULL);
 
 	index = key_index((unsigned char *)key, ht->size);
 
 	if (!ht->array[index])
 		return (NULL);
-	else
-	{
-		node = ht->array[index];
-		while (!node && strcmp(node->key, key))
-			node = node->next;
-		if (!node)
-			return (NULL);
-		else
-			return (node->value);
-	}
 
+	node = ht->array[index];
+	while (!node && strcmp(node->key, key))
+		node = node->next;
+	if (!node)
+		return (NULL);
+
+	return (node->value);
 }
