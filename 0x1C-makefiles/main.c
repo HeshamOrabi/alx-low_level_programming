16f6474293e0842b4914197274fea68cf4f0c20f
Hesham <Dev@heshamorabi.com>
2023-11-23 01:08:02 +0200

    make -f 0-Makefile

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,13 @@
+#include "make.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	print_school();
+	return (0);
+}
---
+++ 0x1C-makefiles/make.h
@@ -0,0 +1,8 @@
+#ifndef MAKE_H
+#define MAKE_H
+
+#include <stdio.h>
+
+int print_school(void);
+
+#endif
